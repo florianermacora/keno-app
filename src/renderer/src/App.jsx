b36fe783f22a17db2343cc64c43c76c80dce7dec
Florian ERMACORA <f3rmacora@gmail.com>
2024-04-04 09:58:19 +0200

    initialisation du projet

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,16 @@
+import Versions from './components/Versions'
+import electronLogo from './assets/electron.svg'
+import TopBar from './components/TopBar'
+
+function App() {
+  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+<TopBar></TopBar>
+      <h1 className="text-lg">hello World !</h1>
+    </>
+  )
+}
+
+export default App
